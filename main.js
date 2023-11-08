d492138bf4d6a3c9be6d4517ef7330ebdec11003
snwgg <127959845+snwgg@users.noreply.github.com>
2023-11-08 17:58:59 -0300

    sdcsdc

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ for (let i = 0; i < listButtons.length; i++) {
     const elementAudios = listAudios[i];
     element.addEventListener('click', () => {
 
-   elementAudios.play() })
+   elementAudios.play(); });
 }
