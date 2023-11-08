6c9e45b8a8d916cdfe6cb02b0503203487192f1c
snwgg <127959845+snwgg@users.noreply.github.com>
2023-11-08 18:16:42 -0300

    sadsadasd

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const listButtons = document.querySelectorAll(".tecla");
 const listAudios = document.querySelectorAll("audio");
-for (let i = 0; i < listButtons.length; i++) {
-    const element = listButtons[i];
-    const elementAudios = listAudios[i];
-    element.addEventListener('click', () => {
 
-   elementAudios.play(); 
-});
+for (let i = 0; i < listButtons.length; i++) {
+    const button = listButtons[i];
+    const audio = listAudios[i];
+
+    button.addEventListener('click', () => {
+        audio.play();
+    });
 }
