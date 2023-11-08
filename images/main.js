37caf8da91fec8f31b710a40d72ddb464580ddda
snwgg <127959845+snwgg@users.noreply.github.com>
2023-11-08 17:24:36 -0300

    utgctyiwtg

--- images/main.js
+++ images/main.js
@@ -1,11 +1,12 @@
-const listateclas=document.querySelector('.tecla')
-function tocasom(idElemento){
-    document.querySelector(idElemento).play()
+const listaTeclas = document.querySelectorAll ('.tecla')
+function tocaSom(idElemento){
+    document.querySelector(idElemento).play)
 }
-for(let indice = 0; indice < listateclas.length; indice++){
-    const class1 = listateclas[indice].classList[1]
-    const idAudio = '#som_${class1}'
-    listateclas[indice].onclick = function(){
-        tocasom(idAudio)
+
+for(let indice = 0; indice < listaTeclas.length; indice++){
+    const class1 = listaTeclas[indice].classlist[1]
+    const idAudio = `#som_${class1}`
+    
+    listaTeclas[indice].onclick = function){
+        tocaSom(idAudio)
     }
-}
