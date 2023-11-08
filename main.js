518a7a53601117f3b20e38b044942c9af04a1732
snwgg <127959845+snwgg@users.noreply.github.com>
2023-11-08 17:40:49 -0300

    asdasd

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const teclas = document.querySelectorAll('.tecla');
-
-    teclas.forEach(tecla => {
-        tecla.addEventListener('click', function () {
-            const somId = this.id.replace('tecla_', '');
-            tocaSom(somId);
-        });
-    });
-
-    function tocaSom(somId) {
-        const audio = new Audio(`sounds/${somId}.wav`);
-        audio.play();
-    }
-});
+function(){
+    document.querySelector('#som_tecla_pom').play();
+    
+}
