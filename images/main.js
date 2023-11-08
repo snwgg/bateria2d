ce3f8ed629053b7e54513a6464c77dfda9abc624
snwgg <127959845+snwgg@users.noreply.github.com>
2023-11-08 17:26:37 -0300

    rtrtesf

--- images/main.js
+++ images/main.js
@@ -1,12 +1,14 @@
-const listaTeclas = document.querySelectorAll ('.tecla')
-function tocaSom(idElemento){
-    document.querySelector(idElemento).play)
+const listaTeclas = document.querySelectorAll('.tecla');
+
+function tocaSom(idElemento) {
+    document.querySelector(idElemento).play();
 }
 
-for(let indice = 0; indice < listaTeclas.length; indice++){
-    const class1 = listaTeclas[indice].classlist[1]
-    const idAudio = `#som_${class1}`
+for (let indice = 0; indice < listaTeclas.length; indice++) {
+    const class1 = listaTeclas[indice].classList[1]; 
+    const idAudio = `#som_${class1}`;
     
-    listaTeclas[indice].onclick = function){
-        tocaSom(idAudio)
+    listaTeclas[indice].onclick = function() { 
+        tocaSom(idAudio);
     }
+}
